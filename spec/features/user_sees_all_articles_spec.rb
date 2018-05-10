7062246ee70430f70a17e085a6bbff5a1075bfe0
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-10 15:16:46 -0600

    formatted tests

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -4,19 +4,18 @@ describe 'user sees all articles' do
   describe 'they visit /articles' do
     it 'displays all articles' do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 1", body: "Body 1")
 
-      visit articles_path
+      visit '/articles'
 
-      click_link article.title
-
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
+      expect(page).to have_link(article_1.title)
+      expect(page).to have_content(article_2.title)
     end
-
+    
     it 'can click on a new article link' do
       visit '/articles'
 
-      expect(page).to have_link('Create a New Article')
+      expect(page). to have_link('Create a New Article')
     end
   end
 end
